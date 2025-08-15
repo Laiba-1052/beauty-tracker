8b4b6d288df323bc636929518293a66c07eb9861
Laiba-1052 <164543075+Laiba-1052@users.noreply.github.com>
2025-08-15 14:01:55 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-   apiKey: "AIzaSyAahP8SPJaOrAH_gAUekaTvNsU94D3RMdk",
-  authDomain: "beauty-tracker-852f7.firebaseapp.com",
-  projectId: "beauty-tracker-852f7",
-  storageBucket: "beauty-tracker-852f7.firebasestorage.app.com",
-  messagingSenderId: "328204523500",
-  appId: "1:328204523500:web:863e98b868f00394ef52c9"
-};
+//add your api key 
+const firebaseConfig = {}
+ 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
